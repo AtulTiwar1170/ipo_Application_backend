da6ae307403b12d08443942f62177c25e23516d7
AtulTiwar1170 <atult2620@gmail.com>
2025-01-24 20:00:19 +0530

    express urlencoded implemented

--- index.js
+++ index.js
@@ -8,16 +8,23 @@ const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // MongoDB connection
 const connect = require('./db/db.connection')
 
-
-// router
+// Routes
 const userRoutes = require('./routes/user.routes')
 const createIPORoutes = require('./routes/create.IPO.routes')
 const publicRoutes = require('./routes/public.routes')
+
+// Use routes
+app.use('/', publicRoutes)
+app.use('/create', createIPORoutes)
+app.use('/user', userRoutes)
+
+// Error handler
 const errorHandler = (err, req, res, next) => {
     console.error(err.stack);
     res.status(err.status || 500).json({
@@ -25,15 +32,7 @@ const errorHandler = (err, req, res, next) => {
         message: err.message || 'Internal Server Error'
     });
 };
-
-
-// error handler
 app.use(errorHandler);
-
-// Routes
-app.use('/', publicRoutes )
-app.use('/create', createIPORoutes)
-app.use('/user', userRoutes)
 
 // Start server
 const PORT = process.env.PORT || 3000;
